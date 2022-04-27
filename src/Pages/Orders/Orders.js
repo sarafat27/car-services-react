f0eadb38dd4e5b0723685ca9cf6317c2633433ae
sarafat27 <sarafat2769@gmail.com>
2022-04-27 18:08:08 +0600

    jwt implementation completed

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,21 +1,29 @@
-import axios from 'axios';
+import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import axiosPrivate from '../../API/axiosPrivate';
 import auth from '../../firebase.init';
 
 const Orders = () => {
     const [orders, setOrders] = useState([]);
     const [user] = useAuthState(auth);
+    const navigate = useNavigate();
     useEffect(() => {
         const getOrders = async () => {
             const email = user.email
             const url = `http://localhost:5000/order?email=${email}`
-            const { data } = await axios.get(url, {
-                headers: {
-                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            try {
+                const { data } = await axiosPrivate.get(url);
+                setOrders(data)
+            }
+            catch (error) {
+                console.log(error);
+                if (error.response.status === 401 || error.response.status === 403) {
+                    signOut(auth)
+                    navigate('/login')
                 }
-            });
-            setOrders(data)
+            }
         }
         getOrders();
     }, [])
